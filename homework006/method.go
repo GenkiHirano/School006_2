9b39ac5914005f901c6980349d6104d852f71d2d
nrhirano <82010794+nrhirano@users.noreply.github.com>
2021-11-09 13:36:53 +0900

    Update method.go

--- homework006/method.go
+++ homework006/method.go
@@ -9,9 +9,6 @@ type Triangle struct {
 
 func (t Triangle) Test_3() int {
 	fmt.Println("homework006_3")
-	// 第6回スクール 宿題3 メソッドを実装してください。
-	// Test_3関数をメソッドにしてください。
-	//t := Triangle{Bottom: 25, Height: 20}
-	//fmt.Println("Triangle: ", t.Triangle())
+	
 	return t.Bottom * t.Height / 2
 }
